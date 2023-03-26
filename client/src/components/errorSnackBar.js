336ec4593988d8a3bc1ff430682d7568236e6955
Subham <sc88dev0028@gmail.com>
2023-03-26 16:12:28 +0530

    Update Packages and Added Snack Bar

--- client/src/components/errorSnackBar.js
+++ client/src/components/errorSnackBar.js
@@ -1,9 +1,26 @@
+import React from "react";
 import { useErrorContext } from "../hooks/errorContext";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const capitalize = (input) => {
+  return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+};
 
 const ErrorSnackBar = () => {
   const { errorMessage } = useErrorContext();
 
-  return errorMessage?.length > 0 && <div>{errorMessage}</div>;
+  return (
+    errorMessage?.length > 0 && (
+      <Snackbar open={errorMessage?.length > 0}>
+        <Alert severity="error">{capitalize(errorMessage)}</Alert>
+      </Snackbar>
+    )
+  );
 };
 
 export default ErrorSnackBar;
